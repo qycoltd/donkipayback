6b069c420086e7e4c5a20f6adb58d23521ca064a
qycoltd <qy.co.ltd@gmail.com>
2026-04-15 15:04:34 +0900

    Add files via upload

---
+++ donkipayback1/src/pages/ElectronicsPage.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import CouponTopNav from '../components/CouponTopNav';
+import CouponGrid from '../components/CouponGrid';
+import { electronicsCoupons } from '../data/couponData';
+
+export default function ElectronicsPage() {
+  useEffect(() => {
+    document.title = '전자상가 쿠폰';
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-white font-sans text-gray-900">
+      <CouponTopNav />
+      <main className="max-w-2xl mx-auto px-4 py-8">
+        <h1 className="text-2xl font-bold mb-6">전자상가 쿠폰</h1>
+        <CouponGrid brands={electronicsCoupons} />
+      </main>
+    </div>
+  );
+}
